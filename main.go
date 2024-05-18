8d3f154829dc0b7bfac50753fd26531d3462f1fd
Justin Davison <justin@macbook-pro.mynetworksettings.com>
2024-05-18 07:57:07 -0400

    Adding timeout, documentation, and refactoring for code cleanliness

--- main.go
+++ main.go
@@ -5,56 +5,93 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"time"
 
 	htgotts "github.com/hegedustibor/htgo-tts"
 	voices "github.com/hegedustibor/htgo-tts/voices"
 )
 
-// TO DO:
-// 1. Implement optional timer on answering questions
-// 2. Implement stats report after the quiz is finished.
-// 3. Clean up code.
+// AudioData represents the data required to play audio.
+type AudioData struct {
+	AudioFilePath string // The file path of the audio file to be played.
+	NumberToRead  int    // The number to be read in the audio.
+}
+
+// QuizData represents the data required for the quiz.
+type QuizData struct {
+	QuizSize           int // The size of the quiz.
+	CorrectAnswerCount int // The count of correct answers.
+	QuizWaitTime       int // The time to wait for the user to answer.
+}
 
 func main() {
+	answerChannel := make(chan int)
+	resultsChannel := make(chan QuizData)
+	quizData := QuizData{QuizSize: 5, QuizWaitTime: 10}
 
-	var audioFolderPath = "audio"
-	var quizSize = 5
-	var correctAnswerCount = 0
-	var userInput int
+	go RunQuiz(resultsChannel, answerChannel, quizData)
+	go PollAnswers(answerChannel)
 
-	for i := 0; i < quizSize; i++ {
+	quizResults := <-resultsChannel
+	fmt.Printf("You got %d out of %d correct!\n", quizResults.CorrectAnswerCount, quizResults.QuizSize)
+}
+
+// RunQuiz loops through the quiz and plays audio for the user to listen to.
+// At the end of the quiz, the results are sent to the resultsChannel and the resultsChannel is closed.
+func RunQuiz(resultsChannel chan<- QuizData, answerChannel <-chan int, quizData QuizData) {
+	for i := 0; i < quizData.QuizSize; i++ {
 		numberToRead := rand.Intn(9999) + 1
-		PlayAudio(audioFolderPath, numberToRead)
+		PlayAudio(AudioData{NumberToRead: numberToRead})
+		fmt.Print("Enter the number you heard: ")
 
-		for {
-			fmt.Print("Enter the number you heard: ")
-			fmt.Scan(&userInput)
-			if userInput == numberToRead {
-				fmt.Println("Correct!")
-				correctAnswerCount++
-				break
-			} else if userInput == -1 {
-				fmt.Println("Retrying?")
-				speech := htgotts.Speech{Folder: audioFolderPath, Language: voices.Japanese}
-				res := speech.PlaySpeechFile("audio/current_question.mp3")
-				if res != nil {
-					fmt.Println("Error playing speech file: ", res)
-				}
-			} else {
-				fmt.Println("Incorrect.")
-				break
+		// Wait for the user to answer or for the time to run out.
+		// Either outcome results in the quiz moving to the next question.
+		select {
+		case answer := <-answerChannel:
+			if answer == numberToRead {
+				quizData.CorrectAnswerCount++
 			}
+		case <-time.After(time.Duration(quizData.QuizWaitTime) * time.Second):
+			fmt.Println("Time's up! Moving to the next question.")
 		}
-		os.RemoveAll(audioFolderPath)
+		// Clear the audio file after each question to ensure
+		// subsequent questions do not play the same audio.
+		// This doesn't seem optimal, but I'm not sure if the htgotts package
+		// allows for overwriting an audio file upon creating a new file of the same name.
+		os.RemoveAll("audio")
+	}
+	// Sending on the resultsChannel signifies the end of the quiz.
+	resultsChannel <- quizData
+	close(resultsChannel)
+}
+
+// PollAnswers polls for user input and sends the answer to the answerChannel.
+// If the user enters -1, the current question is replayed.
+func PollAnswers(answerChannel chan<- int) {
+	var userInput int
+	for {
+		fmt.Scan(&userInput)
+		if userInput == -1 {
+			PlayAudio(AudioData{AudioFilePath: "audio/current_question.mp3"})
+		} else {
+			answerChannel <- userInput
+		}
 	}
 }
 
-func PlayAudio(audioFolderPath string, numberToRead int) {
+// PlayAudio uses the htgotts package to create mp3 files of the numbers being read and then plays those mp3 files.
+func PlayAudio(audioData AudioData) {
+	var audioFolderPath = "audio"
 	speech := htgotts.Speech{Folder: audioFolderPath, Language: voices.Japanese}
-	filename, error := speech.CreateSpeechFile(strconv.Itoa(numberToRead), "current_question")
-	if error != nil {
-		fmt.Println("Error creating speech file: ", error)
-		return
+	// The AudioFilePath attribute being blank signifies a new question,
+	// where the attribute being populated represents a question being replayed.
+	if audioData.AudioFilePath == "" {
+		filename, error := speech.CreateSpeechFile(strconv.Itoa(audioData.NumberToRead), "current_question")
+		if error != nil {
+			fmt.Println("Error creating speech file: ", error)
+			return
+		}
+		audioData.AudioFilePath = filename
 	}
-	speech.PlaySpeechFile(filename)
+	speech.PlaySpeechFile(audioData.AudioFilePath)
 }
